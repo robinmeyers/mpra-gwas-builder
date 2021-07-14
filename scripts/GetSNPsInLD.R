e173f21f4573aa08e820955d625a228a44d586b5
robinmeyers <robin.m.meyers@gmail.com>
2021-07-14 12:41:11 -0400

    bug fix

--- scripts/GetSNPsInLD.R
+++ scripts/GetSNPsInLD.R
@@ -201,11 +201,13 @@ if (!is.null(snakemake@config$gwas_pop_key)) {
     index_snps_pop_match <- index_snps_cleaned %>%
         left_join(study_key_table %>%
                       mutate(pop = str_split(code, ",")) %>%
-                      unnest(pop) %>% distinct(pubmed, pop)) %>%
-        group_by(disease, gwas_snp, index_snp, coord_b38, coord_b37, pubmed, sample) %>%
-        summarise(pops = paste0(sort(pop), collapse=","))
+                      unnest(pop) %>% distinct(pubmed, pop))
 
-    write_tsv(index_snps_pop_match, "outs/gwas_study_index_snps_matched_populations.tsv")
+
+    index_snps_pop_match %>%
+        group_by(disease, gwas_snp, index_snp, coord_b38, coord_b37, pubmed, sample) %>%
+        summarise(pops = paste0(sort(pop), collapse=",")) %>%
+        write_tsv("outs/gwas_study_index_snps_matched_populations.tsv")
 
     index_snps_pop_match %>%
         distinct(disease, pubmed, sample, pops) %>%
